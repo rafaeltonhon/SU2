ef093a0cb0880e54fee1d778d11fa5a0ea159a2a
rafaeltonhon <tonhonr@usp.br>
2024-08-13 13:06:47 -0300

    The dinamical memory allocation was removed, improving the performance.

--- su2main.f90
+++ su2main.f90
@@ -9,23 +9,23 @@ program su2main
 
     call system_clock(count_rate=rate)
     call system_clock(itimes1)
-    open(unit=1,file='su2-in.dat')
-    read(1,*) nr, nt            ! lattice size space time
-    read(1,*) beta              ! beta
-    read(1,*) nmc, nterm        ! number of monte carlo sweeps number of thermalization sweeps
-    read(1,*) a, b              ! maximum size of the wilson loops we want
-    read(1,*) iprint            ! if 1 we print the configurations on the file suN-lattice
-    read(1,*) ncorr             ! number of discarted configurations
-    read(1,*) tol               ! gauge fix tolerance
-    read(1,*) initflag          ! init flag 1-> cold init, 2-> hot init
-    read(1,*) function          ! function a ->  termilize the lattice and find the correlation time
+!    open(unit=1,file='su2-in.dat')
+!    read(1,*) nr, nt            ! lattice size space time
+!    read(1,*) beta              ! beta
+!    read(1,*) nmc, nterm        ! number of monte carlo sweeps number of thermalization sweeps
+!    read(1,*) a, b              ! maximum size of the wilson loops we want
+!!    read(1,*) iprint            ! if 1 we print the configurations on the file suN-lattice
+!    read(1,*) ncorr             ! number of discarted configurations
+!    read(1,*) tol               ! gauge fix tolerance
+!    read(1,*) initflag          ! init flag 1-> cold init, 2-> hot init
+!    read(1,*) function          ! function a ->  termilize the lattice and find the correlation time
                                 ! b -> termilize the lattice and generate uncorrelated configurations
                                 ! c -> read the configurations and make measures
     close(1)
 
-    allocate(u(nr,nr,nr,nt,4,4),r(nr,nr,nr,nt,4,4))
-    allocate(ug(nr,nr,nr,nt,4,4),z(nr,nr,nr,nt,4,4))
-    allocate(w(a,b),wv(a,b),wr(a,b),we(a,b),wo(a,b))
+    !allocate(u(nr,nr,nr,nt,4,4),r(nr,nr,nr,nt,4,4))
+    !allocate(ug(nr,nr,nr,nt,4,4),z(nr,nr,nr,nt,4,4))
+    !allocate(w(a,b),wv(a,b),wr(a,b),we(a,b),wo(a,b))
     call init(u,beta,initflag)
 
     select case(function)
@@ -52,18 +52,30 @@ program su2main
             call maximal_center_gauge(u,ug,tol)
             call centerprojection(ug,z)
             call centerremotion(ug,r)
-            call wilsonvortex(ug,r,z,w,wr,wv,we,wo,a,b)
-            call vortex_plot_ss(z,i)
+            call wilsonvortex(ug,r,z,w,wr,wv,a,b)
+            call vortexsperatedwilson(u,z,a,b,we,wo)
+            call vortexcomponets(w,we,wo,a,b)
+            call pvortexdensity(z,a,b)
+           !call vortexlimitedwilson(u,z,a,b)
+           !call vortex_plot_ss(z,i)
             
             ! compute the propagators in the landau gauge
             call landau_gauge(u,ug,tol/1e11) ! full ensemble
             call gluonpropagator(ug,1000) ! complete
             call gluon_formfactor(ug,1100)
 
+            !call centerlandau_gauge(z,z,tol/1e11)
+            !call centergluonpropagator(z,2000)
+            !call centergluon_formfactor(ug,2100)
+
             call landau_gauge(r,r,tol/1e11) ! removed ensenmble
             call gluonpropagator(r,3000)
             call gluon_formfactor(r,3100)
-            
+        
+          	!call measurewilson(u,a,b,w)
+	        !do j=1,a
+	            !write(100+j,*)	i, w(j,:)
+            !enddo
             ! get the next uncorrelated configurations
             do j=1,ncorr ! number of correlated configurations
                 ! discart ncorr configurations
@@ -80,11 +92,35 @@ program su2main
             !call centerremotion(ugauge,ur)
             !call wilsonvortex(ugauge,ur,z,w,wr,wv,we,wo,a,b)
         enddo
+    case('d') 
+        do i=1,nterm
+            call hbstep(u,beta)
+        enddo
+            do j=1,100 ! number of correlated configurations
+                call cooling(u,15)
+                call hbstep(u,beta)
+            enddo
+            do i=1,nmc
+                ! save the actual configuration
+                print*,'Configuration',i
+                if(iprint.eq.1)call save_lattice(u,nr,i)
+                
+                call cooling(u,10)
+                call measurewilson_asym(u,a,b,w)
+                do j=1,a
+                    write(100+j,*) w(j,:)
+                enddo
+                ! get the next uncorrelated configurations
+                do j=1,ncorr ! number of correlated configurations
+                    ! discart ncorr configurations
+                    call hbstep(u,beta)
+                enddo
+            enddo
     case default
         stop 'no valid case was selected'
     endselect
 
-    deallocate(u,ug,z,r,wr,w,wv)
+    !deallocate(u,ug,z,r,wr,w,wv)
     call system_clock(itimes2)
     print*,"cpu time [seconds]: ", (itimes2-itimes1)/float(rate)!(end-start)/60
     write(9000,*) "cpu time [seconds]: ", (itimes2-itimes1)/float(rate)
